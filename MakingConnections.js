e6bc4089d1dabca0afc6a87ea8ede0e05e5e68e4
Invictus2345 <101741265+Invictus2345@users.noreply.github.com>
2022-05-12 19:39:14 -0500

    Add files via upload

---
+++ MakingConnections.js
@@ -0,0 +1,10 @@
+function change(){
+    var changename= document.querySelector('#profile_name');
+    var name= "Joe Scott";
+    changename.innerText= name;
+}
+
+function remove(element){
+    var remove = document.querySelectorAll;
+    element.remove();
+}
